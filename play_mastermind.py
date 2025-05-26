2e9e11a17ec95b5f0238ef5fe12e0d6c7d40ce45
Erik Göransson Gaspar <erikedwardgoransson@gmail.com>
2025-05-26 17:58:12 +0200

    Feat: added command 'best' to play_mastermind.py to show the top moves

--- play_mastermind.py
+++ play_mastermind.py
@@ -1,27 +1,45 @@
 from mastermind import *
 
-key_len = 5
-num_colors = 8
+key_len = 4
+num_colors = 6
 
 #print("Let's play Mastermind! Do you want to set the secret key yourself (1), or play with a random one (2)?")
-print(f"Let's play Mastermind! I have selected a secret key, {key_len} pegs long with {num_colors} colors.")
+print(f"Let's play Mastermind! I have selected a secret key, {key_len} pegs long with {num_colors} colors. Type 'best' to see the optimal moves.")
 
 secret_key = random_key(key_len=key_len, num_colors=num_colors)
-try:
-    while True:
-        guess = Key(input('Guess: '))
-        if len(guess) != 5:
-            print(f'The key is five {key_len} long and so should your guess be — try again')
+history = []
+while True:
+    try:
+        guess = input('> ')
+        if guess == 'best':
+            num_guesses = 5
+            _, guess_entropy = best_guess(history)
+            if len(guess_entropy) == 1:
+                print(f'The only viable guess is {list(guess_entropy.items())[0][0].string}.')
+                continue
+            guess_entropy = sorted(guess_entropy.items(), key=lambda x: x[1], reverse=True)[:num_guesses]
+            print('Best guesses:')
+            for guess, entropy in guess_entropy:
+                print(f'{guess.string}: {entropy:.2f}')
             continue
-        if max(guess.key) > 8:
+
+        guess = Key(guess)
+        resp = response(secret_key=secret_key, guess=guess)
+        history.append((guess, resp))
+        if len(guess) != key_len:
+            print(f'The key is {key_len} digits long and so should your guess be — try again')
+            continue
+        if max(guess.key) > num_colors:
             print(f'There are only {num_colors} in play. Make another guess!')
 
-        if guess == secret_key:
+        if resp == Response([2]*key_len):
             print('Congrats! You win 🎉')
             break
         else:
-            print(f'The Codemaster responds: {response(secret_key=secret_key, guess=guess).string}')
-
-except KeyboardInterrupt:
-    print(f'\nThe secret key was {secret_key.string}.')
-    pass
+            print(f'The Codemaster responds: {resp.string}')
+    except KeyboardInterrupt:
+        print(f'\nThe secret key was {secret_key.string}.')
+        break
+    except TypeError:
+        print('Invalid guess. Try again!')
+        continue
